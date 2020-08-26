723eee858bdb2bcc95aadebd73ee06f71c25450c
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-08-26 14:29:05 +0100

    refactoring

--- Aula02/run.py
+++ Aula02/run.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, Response
 import json
+
+from grupos.blueprint import grupos_routes
 
 
 app = Flask(__name__)
+app.register_blueprint(grupos_routes)
 
-@app.route("")
+
+@app.route("/")
 def index():
     retorno = {
         "app" : "Sistema de mensagens",
-        "version":
+        "version": 1.0
     }
+    
+    return Response(
+        json.dumps(retorno),
+        200,
+        content_type="application/json"
+    )
 
 
 if __name__ == "__main__":
