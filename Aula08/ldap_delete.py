7de195a90b825c08cb09c65304faa6e02c6f3959
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-10-01 18:01:44 +0100

    Create ldap_delete.py

---
+++ Aula08/ldap_delete.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from ldap3 import Server, Connection
+from hashlib import md5
+from binascii import b2a_base64
+
+username="admin"
+password="louvor01"
+
+server =Server("ldap://192.168.1.12:389")
+ldap_con=Connection(
+    server,
+    "cn={},dc=prowene,dc=net".format(username),
+    password
+)
+ldap_con.bind()
+
+
+email = "joao.hummel@4linux.com.br"
+dn = "uid={},dc=prowene,dc=net".format(email)
+
+print(ldap_con.delete(dn))
